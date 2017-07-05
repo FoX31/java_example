fd03c70ebc928ce3b8365596cdb50597027cf470
FoX31 <H-ebreh@mail.ru>
2017-07-05 16:43:06 +0300

    Задание №4

--- src/test/java/ru/stqa/training/selenium/MyFirstTest.java
+++ src/test/java/ru/stqa/training/selenium/MyFirstTest.java
@@ -4,9 +4,15 @@ import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 import org.openqa.selenium.By;
+import org.openqa.selenium.HasCapabilities;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.firefox.FirefoxOptions;
+import org.openqa.selenium.remote.DesiredCapabilities;
 import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.util.concurrent.TimeUnit;
 
 import static org.openqa.selenium.support.ui.ExpectedConditions.titleIs;
 
@@ -20,16 +26,18 @@ public class MyFirstTest {
 
     @Before
   public void start() {
-      driver = new ChromeDriver();
+      FirefoxOptions options = new FirefoxOptions().setLegacy(true);
+      //options.setBinary("C:\\Program Files\\Nightly\\firefox.exe");
+      WebDriver driver = new FirefoxDriver(options);
+      System.out.println(((HasCapabilities) driver).getCapabilities());
       wait = new WebDriverWait(driver, 10);
     }
 
     @Test
-     public void myFirstTest(){
-      driver.get("http://localhost:8080/litecart/admin/login.php");
-      driver.findElement(By.name("username")).sendKeys("admin");
-      driver.findElement(By.name("password")).sendKeys("admin");
-      driver.findElement(By.name("login")).click();
+     public void myFirstTest() {
+      driver.get("http://www.google.com");
+      driver.findElement(By.name("q")).sendKeys("webdriver");
+      driver.findElement(By.name("btnG")).click();
     }
 
     @After
