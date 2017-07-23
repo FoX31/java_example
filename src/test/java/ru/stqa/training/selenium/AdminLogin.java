58325c3515ea0193be57aa59437d8d0e2fb59aaa
FoX31 <h-ebreh@mail.ru>
2017-07-23 20:30:40 +0300

    Задание 9

--- src/test/java/ru/stqa/training/selenium/AdminLogin.java
+++
@@ -1,8 +0,0 @@
-package ru.stqa.training.selenium;
-
-/**
- * Created by e.kutsenko on 23.07.2017.
- */
-public class AdminLogin {
-
-}
--- src/test/java/ru/stqa/training/selenium/StartTest.java
+++ src/test/java/ru/stqa/training/selenium/StartTest.java
@@ -10,7 +10,11 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
+import java.util.concurrent.TimeUnit;
 
 
 /**
@@ -37,6 +41,58 @@ public class StartTest {
 
     public void openMainPage(){
         driver.navigate().to("http://localhost:8080/litecart/en/");
+    }
+
+    @Test
+    public void test9_1() throws InterruptedException {
+        loginAdmin();
+        driver.findElement(By.xpath("//span[.='Countries']")).click();
+        List<WebElement> lines = driver.findElements(By.xpath("//tr[@class='row']"));
+        List<String> names = new ArrayList();
+        for(int i = 0; i < lines.size(); i++){
+            names.add(lines.get(i).findElement(By.xpath(".//td[5]/a")).getText());
+        }
+        List<String> namesSorted = new ArrayList<String>(names);
+        java.util.Collections.sort(namesSorted);
+        Assert.assertEquals(namesSorted, names);
+        List<String> zones = new ArrayList<String>();
+        for (int i = 0; i < lines.size(); i++){
+            if(Integer.parseInt(lines.get(i).findElement(By.xpath(".//td[6]")).getText()) != 0){
+                zones.add(lines.get(i).findElement(By.xpath(".//td[3]")).getText());
+            }
+        }
+        for (int i = 0; i < zones.size(); i++){
+            driver.findElement(By.xpath("//tr[@class='row']/td[3][.='"+zones.get(i) +"']")).findElement(By.xpath("..//td[5]/a")).click();
+            List<WebElement> linesZones = driver.findElements(By.xpath("//table[@id='table-zones']//tr"));
+            List<String> namesZones = new ArrayList<String>();
+            for (int a = 1; a < linesZones.size() - 1; a++){
+                namesZones.add(linesZones.get(a).findElement(By.xpath(".//td[3]")).getText());
+            }
+            List<String> namesZoneaSorted = new ArrayList<String>(namesZones);
+            java.util.Collections.sort(namesZoneaSorted);
+            Assert.assertEquals(namesZoneaSorted, namesZones);
+            driver.findElement(By.xpath("//span[.='Countries']")).click();
+        }
+    }
+
+    @Test
+    public  void test9_2() throws InterruptedException {
+        loginAdmin();
+        driver.findElement(By.xpath("//span[.='Geo Zones']")).click();
+        int cuntriesCount = driver.findElements(By.xpath("//tr[@class='row']")).size();
+        for(int i = 1; i <= cuntriesCount; i++) {
+            WebElement lines = driver.findElement(By.xpath("//tr[@class='row'][" + i + "]"));
+            lines.findElement(By.xpath(".//td[5]/a")).click();
+            List<WebElement> lineZones = driver.findElements(By.xpath("//table[@class='dataTable']//tr"));
+            List<String> names = new ArrayList<String>();
+            for (int a = 1; a < lineZones.size() - 1; a++) {
+                names.add(lineZones.get(a).findElement(By.xpath("./td[3]//option [@selected='selected']")).getText());
+            }
+            List<String> namesSorted = new ArrayList<String>(names);
+            java.util.Collections.sort(namesSorted);
+            Assert.assertEquals(namesSorted, names);
+            driver.findElement(By.xpath("//span[.='Geo Zones']")).click();
+        }
     }
 
     @Test
